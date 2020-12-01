1cdcfa618121fda49158bf57794cd8611c255b7d
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-12-01 20:33:25 +0700

    feat: add product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,4 @@
-import { Products } from "../database/models";
-import { getAllProductDatabase } from "../models/productModels";
+import { getAllProductDatabase, addNewProductDatabase } from "../models/productModels";
 
 export const getAllProductPage = async (req, res) => {
 
@@ -12,13 +11,27 @@ export const getAllProductPage = async (req, res) => {
 export const addNewProduct = (req, res) => {
 
     // code ở đây...........
-    res.render("home/index");
+    res.render("product/product-add");
 }
 
-export const addNewProductPost = (req, res) => {
+export const addNewProductPost = async (req, res) => {
 
     // code ở đây...........
-    res.render("home/index");
+    const productId = req.body.productId || "1x031";
+    const name = req.body.name || "";
+    // console.log(name);
+    const originPrice = req.body.originPrice || 100000;
+    const salePrice = req.body.salePrice || 99000;
+    const quantity = req.body.quantity || 10;
+    const description = req.body.description || "Không có";
+    const branch = req.body.branch || "Không";
+    const size = req.body.size || "M";
+    const color = req.body.color || "Đen";
+
+    await addNewProductDatabase(productId, name, originPrice, salePrice, quantity, description, branch, size, color);
+
+    const list = await getAllProductDatabase();
+    res.render("product/product-list", { list });
 }
 
 export const editProduct = (req, res) => {
--- models/productModels.js
+++ models/productModels.js
@@ -1,6 +1,23 @@
 import { Products } from "../database/models";
+import { v4 as uuid } from "uuid";
 
 export const getAllProductDatabase = async () => {
     const product = await Products.findAll();
     return product;
+}
+
+export const addNewProductDatabase = async (productId, name, originPrice, salePrice, quantity, description, branch, size, color) => {
+    await Products.create({
+        id: uuid(),
+        productId: productId,
+        name: name,
+        originPrice: originPrice,
+        salePrice: salePrice,
+        quantity: quantity,
+        description: description,
+        branch: branch,
+        size: size,
+        color: color,
+        type: 1
+    });
 }
