1b6b418c3d553730fcb1ff9ef3769a58a46c7736
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-12-07 00:51:52 +0700

    fix: catch async

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,76 +1,80 @@
 import { getAllProductDatabase, addNewProductDatabase, deleteProductDatabase, getOneProductDatabase, updateProductDatabase } from "../models/productModels";
+import catchAsync from "../libs/catchAsync";
 
-export const getAllProductPage = async (req, res) => {
+export const getAllProductPage = catchAsync(
+    async (req, res) => {
 
-    // code ở đây...........
+        const list = await getAllProductDatabase();
+        res.render("product/product-list", { list });
+    }
+);
 
-    const list = await getAllProductDatabase();
-    res.render("product/product-list", { list });
-}
 
-export const addNewProduct = (req, res) => {
+export const addNewProduct = catchAsync(
+    async (req, res) => {
+        res.render("product/product-add");
+    }
 
-    // code ở đây...........
-    res.render("product/product-add");
-}
+);
+export const addNewProductPost = catchAsync(
+    async (req, res) => {
 
-export const addNewProductPost = async (req, res) => {
+        const productId = req.body.productId || "1x031";
+        const name = req.body.name || "";
+        // console.log(name);
+        const originPrice = req.body.originPrice || 100000;
+        const salePrice = req.body.salePrice || 99000;
+        const quantity = req.body.quantity || 10;
+        const description = req.body.description || "Không có";
+        const branch = req.body.branch || "Không";
+        const size = req.body.size || "M";
+        const color = req.body.color || "Đen";
 
-    // code ở đây...........
-    const productId = req.body.productId || "1x031";
-    const name = req.body.name || "";
-    // console.log(name);
-    const originPrice = req.body.originPrice || 100000;
-    const salePrice = req.body.salePrice || 99000;
-    const quantity = req.body.quantity || 10;
-    const description = req.body.description || "Không có";
-    const branch = req.body.branch || "Không";
-    const size = req.body.size || "M";
-    const color = req.body.color || "Đen";
+        await addNewProductDatabase(productId, name, originPrice, salePrice, quantity, description, branch, size, color);
 
-    await addNewProductDatabase(productId, name, originPrice, salePrice, quantity, description, branch, size, color);
+        const list = await getAllProductDatabase();
+        res.render("product/product-list", { list });
+    }
+);
 
-    const list = await getAllProductDatabase();
-    res.render("product/product-list", { list });
-}
+export const editProduct = catchAsync(
+    async (req, res) => {
 
-export const editProduct = async (req, res) => {
+        const id = req.params.id;
+        const product = await getOneProductDatabase(id);
+        // console.log(product);
+        res.render("product/product-edit", { product });
+    }
+);
 
-    // code ở đây...........
-    const id = req.params.id;
-    const product = await getOneProductDatabase(id);
-    // console.log(product);
-    res.render("product/product-edit", { product });
-}
+export const editProductPost = catchAsync(
+    async (req, res) => {
 
-export const editProductPost = async (req, res) => {
+        const id = req.params.id;
 
-    // code ở đây...........
+        const productId = req.body.productId || "1x031";
+        const name = req.body.name || "";
+        const originPrice = req.body.originPrice || 100000;
+        const salePrice = req.body.salePrice || 99000;
+        const quantity = req.body.quantity || 10;
+        const description = req.body.description || "Không có";
+        const branch = req.body.branch || "Không";
+        const size = req.body.size || "M";
+        const color = req.body.color || "Đen";
 
-    const id = req.params.id;
+        await updateProductDatabase(id, productId, name, originPrice, salePrice, quantity, description, branch, size, color);
 
-    const productId = req.body.productId || "1x031";
-    const name = req.body.name || "";
-    const originPrice = req.body.originPrice || 100000;
-    const salePrice = req.body.salePrice || 99000;
-    const quantity = req.body.quantity || 10;
-    const description = req.body.description || "Không có";
-    const branch = req.body.branch || "Không";
-    const size = req.body.size || "M";
-    const color = req.body.color || "Đen";
+        const list = await getAllProductDatabase();
+        res.render("product/product-list", { list });
+    }
+);
 
-    await updateProductDatabase(id, productId, name, originPrice, salePrice, quantity, description, branch, size, color);
-
-    const list = await getAllProductDatabase();
-    res.render("product/product-list", { list });
-}
-
-export const deleteProduct = async (req, res) => {
-    // code ở đây...........
-
-    const id = req.params.id;
-    // console.log(id);
-    await deleteProductDatabase(id);
-    const list = await getAllProductDatabase();
-    res.render("product/product-list", { list });
-}
+export const deleteProduct = catchAsync(
+    async (req, res) => {
+        const id = req.params.id;
+        // console.log(id);
+        await deleteProductDatabase(id);
+        const list = await getAllProductDatabase();
+        res.render("product/product-list", { list });
+    }
+)
