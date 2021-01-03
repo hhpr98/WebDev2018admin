f986cdc30f33dbf12e36cda375dbd7049ae83479
lovinu99 <nhdanh2604@gmail.com>
2021-01-04 00:28:29 +0700

    feat: add login layout, apart of admin authenticate

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ import indexRouter from "./routes/index";
 var app = express(); // express
 dotenv.config(); // using dotenv file
 
+//  authenticate
+
+import expressSession from "express-session";
+import passport from "passport";
+
+app.use(expressSession({secret: "userCookieSecret",resave: false, saveUninitialized: false, cookie:{maxAge: 3000000}})); // 5p ton tai
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,26 +1,103 @@
+import {
+  getAccountByID,
+  updateUserImage,
+  getAccountByUserName,
+  addAccount,
+  updateUserInfoById,
+  updatePasswordById,
+  getNPIById,
+  getAdminAccount
+} from "../models/accountModels";
+import bcrypt from "bcrypt";
 
-import { getAccountByID, updateUserInfo} from "../models/accountModel";
-
-
+const SALT_ROUNDS = 10;
 import catchAsync from "../libs/catchAsync";
 
-export const getAccountPage = catchAsync(
-    async (req, res) => {
-        const id = "14653d9c-95c3-482f-b4cf-f7b15ddff46c";
-        const users = await getAccountByID(id);
-        res.render("account/info", { title: "Account", user:users});
-    }
-  );
+// get layout
+export const getLoginPage = catchAsync(
+  async (req, res) => {
+    // var noti = req.flash('loginMessage')[0];
+    var message = req.session.valid;
+    req.session.valid = null;
+    res.render("home/login", { title: "Đăng nhập", layout: "layout/loginlayout" , Noti:message});
+  }
+);
 
-  export const updateInfo = catchAsync(
-    async(req, res) => {
-      //
-        // thay đổi id này sau
-      const id = '14653d9c-95c3-482f-b4cf-f7b15ddff46c';
-      //
-      await updateUserInfo(req.body.userName, req.body.userPhone, req.body.userEmail, req.body.userAddress, id);
-        const users = await getAccountByID(id);
-        res.redirect("/account");
-      }
-  )
-  
+export const getAccountPage = catchAsync(
+  async (req, res) => {
+    var message = req.session.valid;
+    req.session.valid = null;
+    // lay tam thong tin acc
+    const id = req.user;
+    const user_info = await getAccountByID(id);
+    res.render("account/account", { title: "Tài khoản",layout: "layout/loginlayout", user: user_info, noti:message});
+  }
+);
+
+// get
+export const getAccountAuthenticate = async (accountName) => {
+  const pw = await getAdminAccount(accountName);
+  return pw;
+}
+
+
+// action
+export const logoutAccount = catchAsync(
+  async (req, res) => {
+    req.logout();
+    res.redirect('/');
+  }
+)
+
+// update
+export const updateAvata = catchAsync(
+  async (req, res, filename) => {
+    const id = req.user;
+    const file_src = "/img/uploads/" + filename;
+    await updateUserImage(id, file_src);
+    res.redirect("/account");
+  }
+);
+export const updateInfo = catchAsync(
+  async (req, res) => {
+    const id = req.user;
+    req.session.valid = "Đã cập nhật thông tin tài khoản";
+
+    await updateUserInfoById(req.body.username, req.body.phone, req.body.email, req.body.address, id);
+    res.redirect("/account");
+  }
+)
+export const updatePassword = catchAsync(
+  async (req, res) => {
+    const id = req.user;
+    const account = await getNPIById(id);
+    //
+    if (bcrypt.compareSync(req.body.oldpw, account.pw)){
+      await updatePasswordById(req.body.newpw, id);
+      req.session.valid = "Đã cập nhật mật khẩu";
+    }
+      
+    else{
+      req.session.valid = 'Mật khẩu cũ không khớp!!';
+    }
+      res.redirect("/account");
+  }
+)
+
+////
+
+
+export const postRegisterPage = catchAsync(
+  async (req, res) => {
+    const pass = req.body.pass || "";
+    const repass = req.body.repass || "";
+
+    if (pass !== repass) {
+      res.render("account/register", { title: "Đăng kí", Noti: "Mật khẩu nhập lại không khớp", layout: "layout/loginlayout" });
+      return;
+    }
+    // luu vao db
+    await addAccount(req.body.username, req.body.pass, req.body.email);
+    res.redirect("/login");
+  }
+);
